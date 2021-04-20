98bebe09a0a39711c20961cb41b5ad43f8f8d340
Ablarom99 <71704306+Ablarom99@users.noreply.github.com>
2021-04-20 13:52:07 +0200

    add

---
+++ src/gui/PanelDatosEstudiante.java
@@ -0,0 +1,58 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.GridBagConstraints;
+
+public class PanelDatosEstudiante extends JPanel {
+
+	private JFrame frame;
+	pnlDatosPersonales pnlDatosPersonales = new pnlDatosPersonales();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PanelDatosEstudiante window = new PanelDatosEstudiante();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PanelDatosEstudiante() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		add(pnlDatosPersonales, gbc_lblNewLabel);
+	}
+
+}
