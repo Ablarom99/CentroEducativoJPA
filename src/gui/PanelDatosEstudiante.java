cc950a1fb1bef59f4c35c5815ef94e21af6293e6
Ablarom99 <71704306+Ablarom99@users.noreply.github.com>
2021-04-25 13:10:19 +0200

    add

--- src/gui/PanelDatosEstudiante.java
+++ src/gui/PanelDatosEstudiante.java
@@ -8,6 +8,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import java.awt.GridBagConstraints;
+import javax.swing.JToolBar;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 public class PanelDatosEstudiante extends JPanel {
 
@@ -30,16 +34,52 @@ public class PanelDatosEstudiante extends JPanel {
 	 */
 	private void initialize() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
+		
+		JToolBar toolBar = new JToolBar();
+		GridBagConstraints gbc_toolBar = new GridBagConstraints();
+		gbc_toolBar.insets = new Insets(0, 0, 5, 5);
+		gbc_toolBar.gridx = 0;
+		gbc_toolBar.gridy = 0;
+		add(toolBar, gbc_toolBar);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setIcon(new ImageIcon(PanelDatosEstudiante.class.getResource("/iconos/primen.png")));
+		toolBar.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1.setIcon(new ImageIcon(PanelDatosEstudiante.class.getResource("/iconos/patras.png")));
+		toolBar.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2.setIcon(new ImageIcon(PanelDatosEstudiante.class.getResource("/iconos/palante.png")));
+		toolBar.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("");
+		btnNewButton_3.setIcon(new ImageIcon(PanelDatosEstudiante.class.getResource("/iconos/ultimo.png")));
+		toolBar.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("");
+		btnNewButton_4.setIcon(new ImageIcon(PanelDatosEstudiante.class.getResource("/iconos/guardar.png")));
+		toolBar.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("");
+		btnNewButton_5.setIcon(new ImageIcon(PanelDatosEstudiante.class.getResource("/iconos/nuevo.png")));
+		toolBar.add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("");
+		btnNewButton_6.setIcon(new ImageIcon(PanelDatosEstudiante.class.getResource("/iconos/eliminar.png")));
+		toolBar.add(btnNewButton_6);
 		
 		
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
 		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 0;
+		gbc_lblNewLabel.gridy = 1;
 		add(pnlDatosPersonales, gbc_lblNewLabel);
 	}
 
